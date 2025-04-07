023de83b81178d03502695ccd7f60852039526ef
Vaishnavim <141632505+VaishnaviMehesare@users.noreply.github.com>
2025-04-08 00:42:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-084568db4383264d4" 
+    default     = "ami-07ffb2f4d65357b42" 
 }
 variable "versioning" {
     type        = bool
