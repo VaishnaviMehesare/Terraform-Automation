ffaa8b78ae0da5871321574eb5ffdd3b3a8a7f34
Vaishnavim <141632505+VaishnaviMehesare@users.noreply.github.com>
2025-04-07 23:49:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-samplevaish-batch-27"
+    //bucket = "mydev-project-terraform-samplevaish-batch-27"
+    bucket = "mydev-project-terraform-sample-batch-26"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
